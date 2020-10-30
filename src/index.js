01f2764b0bd3eb6c2420c1de444609120ef97115
Nguyen Minh Hieu <hieu.nguyen7.intern@ntq-solution.com.vn>
2020-10-30 10:03:39 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Myapp from './test1';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Myapp />,
   document.getElementById('root')
 );
 
